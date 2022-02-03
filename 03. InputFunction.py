c36443ef61458ccde622b9fd9cb4d2d9a7c9fb43
Akash Chauhan <akashchauhan.sejapan@gmail.com>
2022-02-03 21:57:24 +0530

    Update 03. InputFunction.py

--- 03. InputFunction.py
+++ 03. InputFunction.py
@@ -2,3 +2,11 @@
 
 s = input("What is yout name? ")
 print(len(s))
+
+
+inp = input()
+name = input('What is Cloudxlab?\n')
+
+question = 'What is 2 multiplied by 3?\n'
+answer = input(question)
+print(int(answer))
